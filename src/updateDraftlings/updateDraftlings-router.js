829f9f4b34f2adf37eabe43b54292c9a899f695d
Angela Bumgarner <amrvitale@gmail.com>
2020-06-21 17:36:19 -0700

    cleaned up updateDraftlings-router.js

--- src/updateDraftlings/updateDraftlings-router.js
+++ src/updateDraftlings/updateDraftlings-router.js
@@ -13,43 +13,25 @@ const serializeDraftling = draftling => ({
     wordcount: xss(draftling.wordcount)
 });
 
-updateDraftlingsRouter
-        .route('/:id')
-        .all((req,res, next) => {
-            updateDraftlingsService
-            .getById(req.app.get('db'),req.params.id)
-
-            .then(draftling => {
-                console.log(draftling);
-                if(!draftling) {
-                    return res.status(404).json({
-                        error: {message: 'Draftling doesn\'t exist.'},
-                    });
-                }
-                res.draftling = draftling;
-                next();
-            })
-            .catch(next);
-        })
-    
-
-    .put(bodyParser, (req, res, next)=> {
-        const {title, wordcount, content} = req.body;
-        const draftlingToUpdate = {title, content, wordcount};
-
-        const numberOfValues = Object.values(draftlingToUpdate).filter(Boolean).length;
+    updateDraftlingsRouter.route("/:id").put(bodyParser, (req, res, next) => {
+        const { title, wordcount, content } = req.body;
+        const draftlingToUpdate = { title, content, wordcount };
+      
+        const numberOfValues = Object.values(draftlingToUpdate).filter(Boolean)
+          .length;
         if (numberOfValues === 0) {
-            return res.status(400).json({
-                error: {
-                    message: 'request body must contain either  \'title\' or \'content\''
-                },
-            });
+          return res.status(400).json({
+            error: {
+              message: "request body must contain either  'title' or 'content'",
+            },
+          });
         }
         updateDraftlingsService
-        .editDraftling(req.app.get('db'),req.params.id,draftlingToUpdate)
-        .then(() => {
+          .editDraftling(req.app.get("db"), req.params.id, draftlingToUpdate)
+          .then(() => {
             res.status(204).end();
-        })
-        .catch(next);
-    });
+          })
+          .catch(next);
+      });
+
     module.exports = updateDraftlingsRouter;
